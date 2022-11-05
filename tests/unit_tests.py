a4227df8d51a8f4c16a1fe7d17b599ff8ec68ff5
Danil1994 <danilaleksandrovich36@gmail.com>
2022-11-05 15:15:03 +0300

    add one comment

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,20 +1,24 @@
 import unittest
+
 from anagrams import anagram
 'исправлено в гит'
 
 class AnagramTest(unittest.TestCase):
-    cases = [
-        ('a1bcd efg!h', 'd1cba hgf!e'),
-        ('abcde', 'edcba'),
-        ('1234', '1234'),
-        ('', ''),
-    ]
-    for text, reversed_text in cases:
-        assert anagram(text) == reversed_text
+    def test_anagram_positive_cases(self):
+        cases = [
+            ('a1bcd efg!h', 'd1cba hgf!e'),
+            ('abcde', 'edcba'),
+            ('1234', '1234'),
+            ('', ''),
+        ]
+        for text, reversed_text in cases:
+            with self.subTest(text=text, reversed_text=reversed_text):
+                self.assertEqual(anagram(text), reversed_text)
+
 
     def test_anagram_negative_cases(self):
-        for i in [123, [1, 2, 3], {'1': 2, 'asd': 1}]:
-            with self.subTest(i=i):
+        for not_str_type in [123, [1, 2, 3], {'1': 2, 'asd': 1}]:
+            with self.subTest(not_str_type=not_str_type):
                 with self.assertRaises(TypeError) as exept_text:
-                    self.assertEqual(anagram(i))
+                    self.assertEqual(anagram(not_str_type))
                 self.assertEqual('You must use only string symbol', str(exept_text.exception))
